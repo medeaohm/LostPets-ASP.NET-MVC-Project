bb5698aaaf366833643ea671209656c4205853e9
medeaohm <nia_omerska@abv.bg>
2016-02-24 12:01:54 +0200

    fixed bug in edit profile

--- Source/Web/LostPets.Web/ViewModels/Profile/EditProfileViewModel.cs
+++ Source/Web/LostPets.Web/ViewModels/Profile/EditProfileViewModel.cs
@@ -12,13 +12,10 @@
 
     public class EditProfileViewModel : IMapFrom<User>, IHaveCustomMappings
     {
-        [Required]
-        [EmailAddress]
         [Display(Name = "Email")]
         [UIHint("EditSingleLineTextDisabled")]
         public string Email { get; set; }
 
-        [Required]
         [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [Display(Name = "Username")]
         [UIHint("EditSingleLineTextDisabled")]
